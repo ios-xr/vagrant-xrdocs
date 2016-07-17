5585982119acf5460f1e143e05cd621e3556aaf8
akshshar <akshshar@cisco.com>
2016-07-17 06:50:59 -0700

    Removed the demo code

--- ospf-iperf-topo/scripts/impair_backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo tc qdisc del dev eth4 root &> /dev/null
-sudo tc qdisc del dev eth3 root &> /dev/null
-echo "Starting packet loss on backup link"
-sudo tc qdisc add dev eth4 root netem loss 4%
--- ospf-iperf-topo/scripts/launch_container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir /misc/app_host/rootfs
-tar -zxf /misc/app_host/scratch/nc_iperf_rootfs.tar.gz -C /misc/app_host/rootfs/
-
-sudo -i virsh create /home/vagrant/nc_iperf.xml
