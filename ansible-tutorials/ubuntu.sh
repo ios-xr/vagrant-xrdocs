0c6975ac9c2b2b9f99ae12a359611f5dbd49d71a
Mike Korshunov <m.korshunov@me.com>
2017-03-04 19:04:59 -0800

    Update ubuntu.sh

--- ansible-tutorials/ubuntu.sh
+++ ansible-tutorials/ubuntu.sh
@@ -13,7 +13,7 @@ mv ~/ansible_hosts  /home/vagrant/iosxr-ansible/remote/ansible_hosts
 mv ~/ansible_env  /home/vagrant/iosxr-ansible/remote/ansible_env
 
 
-
+sudo pip install enum34 idna ipaddress pycparser
 #cd ansible/ && sudo python setup.py install
 cd ansible/ && git checkout stable-2.2 && sudo python setup.py install
 echo "source /home/vagrant/iosxr-ansible/remote/ansible_env" >> /home/vagrant/.profile
